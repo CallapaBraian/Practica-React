6e054723b97b6133ee28b40622dde02843abda89
CallapaBraian <braiancallapa@gmail.com>
2025-09-18 16:28:24 -0300

    prueba 3

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Practica-React/", // EXACTO al nombre del repo
+  base: "/Practica-React/", // <- EXACTO al nombre del repo
 });
